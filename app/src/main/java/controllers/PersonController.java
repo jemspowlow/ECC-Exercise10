0276a605e751c2b2657f0abb3a6ef4db60b6f6c5
James Paolo <jamespaolo.menguito>
2018-09-18 16:27:20 +0800

    Added more logging to PersonController

--- app/src/main/java/controllers/PersonController.java
+++ app/src/main/java/controllers/PersonController.java
@@ -22,102 +22,124 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 @RestController
 class PersonController {
-
+	Logger logger = LoggerFactory.getLogger(PersonController.class);
 	private final PersonService service;
 
 	PersonController(PersonService service) {
 		this.service = service;
+		logger.info("PersonController instantiated.");	
 	}
 
 	// Aggregate root
 	@GetMapping("/persons")
 	List<PersonDTO> all() {
+		logger.info("[GET] /persons");
 		return service.findAll();
 	 }
 	
 	@GetMapping("/persons/{id}/contacts")
 	Page<ContactInfo> getPersonContacts(@PathVariable Long id, Pageable pageable) { 
+		logger.info("[GET] /persons/"+id+"/contacts");
 		return service.findPersonContacts(id,pageable);
 	 }
 	
 	@GetMapping("/persons/{id}/contacts/{contactId}")
 	ContactInfo getPersonContact(@PathVariable Long id, @PathVariable Long contactId) { 
+		logger.info("[GET] /persons/"+id+"/contacts/"+contactId);
 		return service.findPersonContact(id, contactId);
 	 }
 	
 	@GetMapping("/persons/{id}/name")
 	NameOnly getPersonName(@PathVariable Long id) { 
+		logger.info("[GET] /persons/"+id+"/name");
 		return service.findPersonName(id);
 	 }
 	@GetMapping("/persons/{id}/address")
-	AddressDTO getPersonAddress(@PathVariable Long id) { 
+	AddressDTO getPersonAddress(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id+"/address");
 		return service.findPersonAddress(id);
 	 }
 	@GetMapping("/persons/{id}/birthday")
 	BirthDayOnly getPersonBirthDay(@PathVariable Long id) { 
+		logger.info("[GET] /persons/"+id+"/birthday");
 		return service.findPersonBirthDay(id);
 	 }
 	@GetMapping("/persons/{id}/datehired")
 	DateHiredOnly getPersonDateHired(@PathVariable Long id) { 
+		logger.info("[GET] /persons/"+id+"/datehired");
 		return service.findPersonDateHired(id);
 	 }
 	@GetMapping("/persons/{id}/gender")
-	GenderOnly getPersonGender(@PathVariable Long id) { 
+	GenderOnly getPersonGender(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id+"/gender");
 		return service.findPersonGender(id);
 	 }
 	@GetMapping("/persons/{id}/gwa")
-	GwaOnly getPersonGwa(@PathVariable Long id) { 
+	GwaOnly getPersonGwa(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id+"/gwa"); 
 		return service.findPersonGwa(id);
 	 }
 	@GetMapping("/persons/{id}/school")
-	SchoolOnly getPersonSchool(@PathVariable Long id) { 
+	SchoolOnly getPersonSchool(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id+"/school"); 
 		return service.findPersonSchool(id);
 	 }
 	@GetMapping("/persons/{id}/employed")
 	EmployedOnly getPersonEmployed(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id+"/employed");
 		return service.findPersonEmployed(id);
 	 }
 	
 	@GetMapping("/persons/{id}/roles")
-	List<RolesInfo> getPersonRoles(@PathVariable Long id) { 
+	List<RolesInfo> getPersonRoles(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id+"/roles"); 
 		return service.findPersonRoles(id);
 	 }
 	
 	@GetMapping("/persons/{personId}/roles/{roleId}")
 	RolesInfo getPersonRole(@PathVariable Long personId, @PathVariable Long roleId) { 
+		logger.info("[GET] /persons/"+personId+"/roles/"+roleId);
 		return service.findPersonRole(personId,roleId);
 	 }
 	
 	@PostMapping(path="/persons", consumes="application/json;charset=UTF-8")
 	PersonDTO addNewPerson(@RequestBody PersonDTO newPerson) {
+		logger.info("[POST] /persons");
 		return service.add(newPerson);
 	 }
 	
 	@PostMapping(path="/persons/{personId}/contacts", consumes="application/json;charset=UTF-8")
 	ContactDTO addNewPerson(@RequestBody ContactDTO newContact, @PathVariable Long personId) {
+		logger.info("[POST] /persons/"+personId+"/contacts");
 		return service.addContact(newContact, personId);
 	 }
 	
 	@PostMapping(path="/persons/{personId}/roles", consumes="application/json;charset=UTF-8")
 	PersonDTO addARole(@RequestBody RolesDTO role, @PathVariable Long personId) { 
+		logger.info("[GET] /persons/"+personId+"/roles");
 		return service.addRole(role,personId);
 	}
 	
 	// Single item
 	@GetMapping("/persons/{id}")
 	PersonDTO one(@PathVariable Long id) {
+		logger.info("[GET] /persons/"+id);
 		return service.findOne(id);		
 	 }
 	
 	@PutMapping("/persons/{id}") 
-	PersonDTO updatePerson(@RequestBody Person newPerson, @PathVariable Long id) { 
+	PersonDTO updatePerson(@RequestBody Person newPerson, @PathVariable Long id) {
+		logger.info("[PUT] /persons/"+id+""); 
 		return service.updatePerson(newPerson, id);
 	 }
 	
 	@DeleteMapping("/persons/{id}")
 	void deletePerson(@PathVariable Long id) {
+		logger.info("[DELETE] /persons/"+id);
 		service.deleteById(id);
 	 }
 	//TODO: ADD MORE PATCHMAPPING
